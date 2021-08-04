cbfe164c842ec30cef709cbb00a3bc4dd4233dc6
maromaro0013 <maromaro0013@gmail.com>
2021-08-04 18:03:03 +0900

    カテゴリの追加と削除ができるようにした

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -11,6 +11,7 @@ class CategoriesController < ApplicationController
   
     def create
       @category = Category.new(category_params)
+      @category.user_id = current_user.id
       if @category.save
         redirect_to action: :index
       else
@@ -18,11 +19,30 @@ class CategoriesController < ApplicationController
       end
     end
   
+    def show
+      @category = Category.find(params[:id])
+    end
+
     def edit
+      @category = Category.find(params[:id])
+    end
+  
+    def update
+      @category = Category.find(params[:id])
+      if @category.update(category_params)
+        redirect_to @category
+      else
+        render "edit"
+      end
+    end
+  
+    def destroy
+      Category.find(params[:id]).destroy
+      redirect_to "/categories"
     end
   
     private
     def category_params
-      params.require(:category).permit(:title)
+      params.require(:category).permit(:title, :user_id)
     end
   end
